7074c37647e97732942cb4c3b2dffc3902949376
unknown <Wear>
2022-08-04 11:22:34 +0100

    Changed case in source definition

--- models/clone_small_sample_data/clone_small_sample_data__store_sales.sql
+++ models/clone_small_sample_data/clone_small_sample_data__store_sales.sql
@@ -1,6 +1,6 @@
 with source_store_sales as (
-    select * from {{ source('clone_small_sample_data', 'store_data')}}
-)
+    select * from {{ source('clone_small_sample_data', 'store_sales')}}
+),
 
 final as (
     select * from source_store_sales
